7497b8868f9e7fae8db5b54cc4b7e355e7335959
levenew <sayshowplz@qq.com>
2019-02-25 22:25:39 +0800

    init

--- index.js
+++ index.js
@@ -2,13 +2,12 @@
  * @format
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
-
+//看看Git有效果没有
 import {AppRegistry} from 'react-native';
 import AppNavigators from './js/navigator/AppNavigators';
 import {
     createAppContainer
 } from 'react-navigation';
-
 
 import {name as appName} from './app.json';
 
